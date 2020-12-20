63b21ec5256081a029f2f7f1e2c22b4ab1003e88
Brandon Scott <btfs95@gmail.com>
2020-12-20 13:48:50 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+// require routes
+
+// mogo connection
+
+
+// connect to app
+const PORT = 
+
+
+//  boot app
